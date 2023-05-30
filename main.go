9806651550fa41f120036d5c38167be2675a3f45
Turuu <turuu0710@gmail.com>
2023-05-30 20:39:10 +0800

    main.go api added

--- main.go
+++ main.go
@@ -20,4 +20,14 @@ func main() {
 	routes.AuthRoutes(router)
 	routes.userRoutes(router)
 
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.RUN(":" + port)
+
 }
